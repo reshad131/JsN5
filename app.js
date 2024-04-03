4a1914072ec1c7bed9c7345dfbc5cec982066c32
reshad131 <161569107+reshad131@users.noreply.github.com>
2024-04-03 10:18:15 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// let products = [
+//     {
+//         id: 1,
+//         name: "iphone 11",
+//         price: 1000
+//     },
+//     {
+//         id: 2,
+//         name: "iphone 12",
+//         price: 700
+//     },
+//     {
+//         id: 3,
+//         name: "iphone 13",
+//         price: 800
+//     },
+//     {
+//         id: 4,
+//         name: "iphone 14",
+//         price: 900
+//     },
+//     {
+//         id: 5,
+//         name: "iphone 7",
+//         price: 450
+//     },
+//     {
+//         id: 6,
+//         name: "iphone x",
+//         price: 1001
+//     },
+//     {
+//         id: 7,
+//         name: "iphone 13",
+//         price: 1000
+//     },
+//     {
+//         id: 8,
+//         name: "iphone 15",
+//         price: 2900
+//     },
+//     {
+//         id: 9,
+//         name: "iphone 5",
+//         price: 250
+//     },
+//     {
+//         id: 10,
+//         name: "iphone 6",
+//         price: 300
+//     },
+// ]
+
+
+// sort__products = products.sort((a, b) => a.price - b.price);
+// product__names = sort__products.map(product => product.name);
+
+// console.log(product__names );
+
+
+
+// nums = [6, 7, 8, 9, 3];
+
+
+// toplam = 0;
+// for (i = 0; i < nums.length; i++) {
+//     toplam += nums[i];
+// }
+
+// console.log("Cem:", toplam);
